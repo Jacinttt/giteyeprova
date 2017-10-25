0e8774fd43cffb82790ddd2adf0e75c09593b5ef
guest-tw6akb <guest-tw6akb@super-HP-Compaq-6200-Pro-SFF-PC>
2017-10-25 17:05:01 +0200

    contingut git afagit

---
+++ src/practica/git/PRACTICAGIT.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica.git;
+
+/**
+ *
+ * @author guest-jlvzhy
+ */
+public class PRACTICAGIT {
+
+   
+    public static void main(String[] args) {
+        
+        /*contingut prova per el git */
+        
+    }
+    
+    
+    public static void provagit(){
+        
+        String prova ="hola git prova";
+        
+        System.out.println(prova);
+        
+    
+    }
+    
+}
